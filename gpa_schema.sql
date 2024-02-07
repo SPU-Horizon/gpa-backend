7d0b1f9c03aec85fdda6a21db131caab80d54580
Steven Aziz <90646854+azizsteven@users.noreply.github.com>
2024-02-07 15:54:15 -0800

    Updated database schema

--- gpa_schema.sql
+++ gpa_schema.sql
@@ -8,20 +8,19 @@ CREATE TABLE IF NOT EXISTS student (
 student_id SMALLINT UNSIGNED AUTO_INCREMENT,
 first_name VARCHAR(35) NOT NULL,
 last_name VARCHAR(35) NOT NULL,
-email VARCHAR(254) NOT NULL,
+email VARCHAR(254) NOT NULL UNIQUE,
 avatar BLOB,
 enrollment_year YEAR,
 enrollment_quarter ENUM('autumn', 'winter', 'spring', 'summer'),
 graduation_year YEAR,
 graduation_quarter ENUM('autumn', 'winter', 'spring', 'summer'),
-field JSON,
-remaining_courses JSON, -- REVIEW -> store full graduation requirements?
+field_requirements JSON,
 PRIMARY KEY (student_id)
 );
 
 CREATE TABLE IF NOT EXISTS field (
 field_id TINYINT UNSIGNED AUTO_INCREMENT,
-name VARCHAR(72) NOT NULL,
+name VARCHAR(72) NOT NULL UNIQUE,
 type ENUM('major', 'minor', 'program'),
 credits TINYINT UNSIGNED,
 PRIMARY KEY (field_id)
@@ -37,7 +36,7 @@ standing SET('freshman', 'sophomore', 'junior', 'senior') NOT NULL,
 restrictions JSON,
 prerequisites JSON,
 corequisites JSON,
-approval_required BOOL,
+approval_required BOOL DEFAULT FALSE,
 last_offered YEAR,
 recurrence_year ENUM('yearly', 'biyearly', 'occasionally'),
 recurrence_quarter SET('autumn', 'winter', 'spring', 'summer'),
@@ -48,8 +47,8 @@ PRIMARY KEY (course_id)
 CREATE TABLE IF NOT EXISTS section (
 section_id SMALLINT UNSIGNED,
 course_id VARCHAR(10) NOT NULL,
-year YEAR,
-quarter ENUM('autumn', 'winter', 'spring', 'summer'),
+year YEAR NOT NULL,
+quarter ENUM('autumn', 'winter', 'spring', 'summer') NOT NULL,
 topic TINYTEXT,
 classes JSON,
 location TINYTEXT,
