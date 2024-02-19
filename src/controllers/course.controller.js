ffb05d9e9e8eee2f10a6985567b81a15498fd320
Matthew Negasi <negasim@spu.edu>
2024-02-19 13:47:36 -0800

    Fixing the hotfix that was for the bugfix.

--- src/controllers/course.controller.js
+++ src/controllers/course.controller.js
@@ -1,5 +1,6 @@
 import { getEnrollments } from "../../database.js";
 import courseParse from "../GPA HTML Parsing/course_parser.js";
+import fs from "fs/promises";
 
 //This will get all classes a student has taken, what year/quarter it was taken, and what grade was recieved
 //It requires the student's email to be passed in the body of the request
@@ -17,7 +18,14 @@ export async function parseCourses(req, res) {
   console.log("Received file:", file);
 
   const parsedCourses = courseParse(file.path);
-  console.log(parsedCourses);
+  // console.log(parsedCourses);
+
+  try {
+    await fs.unlink(file.path);
+    console.log("File removed successfully.");
+  } catch (error) {
+    console.error("Error removing file:", error);
+  }
 
   // Respond to the client
   res.send("File received successfully");
