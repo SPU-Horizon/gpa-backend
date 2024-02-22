dc910a4767f63fb2863133b211e7594456dd6d5a
Steven Aziz <90646854+azizsteven@users.noreply.github.com>
2024-02-22 12:26:19 -0800

    Feature/implement sql queries (#7)
    * implement getEnrollments
    
    * Update getEnrollments
    
    * Hotfix/ CommonJS -> ESM
    
    * Bugfix/ Fixed ambiguity in SQL query
    
    * Implemented addStudent
    
    * Implement addEnrollments
    
    addEnrollments is a function that accepts an object with attributes for enrollment_year, enrollment_quarter, graduation_year, graduation_quarter, and enrollments, updates student data in the student table, and inserts student enrollments in the enrollment table.
    
    * Add code comments, implement userExists, implement getUser, improve getEnrollments, update addEnrollments
    
    * Fix syntax bug
    
    * Fix syntax bugs
    
    * Update GPA Schema
    
    Set credits attribute to NOT NULL
    
    * Update MySQL Schema and Fix Bugs
    
    * Fix Syntax Bug
    
    * Change data type of avatar from BLOB to TEXT
    
    ---------
    
    Signed-off-by: Steven Aziz <90646854+azizsteven@users.noreply.github.com>

--- database.js
+++ database.js
@@ -7,10 +7,11 @@
  *      Author: Team Horizon
  */
 
-import mysql from "mysql2/promise";
-import dotenv from "dotenv";
-dotenv.config();
+import mysql from 'mysql2/promise'; // mysql2/promise is a MySQL client for Node.js that is written in JavaScript and does not require a compiler. It is a wrapper for mysql2 that uses ES6 promises.
+import dotenv from 'dotenv'; // dotenv is a zero-dependency module that loads environment variables from a .env file into process.env. Storing configuration in the environment separate from code is based on The Twelve-Factor App methodology.
+dotenv.config(); // Load environment variables from a .env file into process.env
 
+// Create a connection pool
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -18,36 +19,175 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME,
 });
 
-// async functions queries to the database written by steven
+const currentYear = new Date().getFullYear();
+const currentQuarter = function() {
+    switch(Math.floor(new Date().getMonth() / 3) + 1) {
+        case 1:
+            return 'winter';
+        case 2:
+            return 'spring';
+        case 3:
+            return 'summer';
+        case 4:
+            return 'autumn';
+        default:
+            return null;
+    };
+}
 
+// check if a user exists
+// accepts an email as parameter
+// returns a boolean indicating whether the user exists
+export async function userExists(email) {
+    const [rows] = await pool.query(`
+        SELECT * FROM student
+        WHERE email = ?
+        `, [email]);
+
+        return rows.length > 0;
+}
+
+// register a new user
+// accepts an object with first_name, last_name, email, and avatar properties
+// returns the student_id of the newly registered user
+export async function registerUser({first_name, last_name, email, avatar}) {
+    const [result] = await pool.query(`
+        INSERT INTO student (first_name, last_name, email, avatar)
+        VALUES (?, ?, ?, ?)
+        `, [first_name, last_name, email, avatar]);
+
+        return result.insertId;
+}
+
+// get a user's information
+// accepts an email as parameter
+// returns an object with student_id, first_name, last_name, email, avatar, graduation_year, and graduation_quarter properties
+export async function getUser(email) {
+    const [[rows]] = await pool.query(`
+        SELECT student_id, first_name, last_name, email, avatar, graduation_year, graduation_quarter
+        FROM student
+        WHERE email = ?
+        `, [email]);
+
+        return rows;
+}
+
+// add enrollments for a student
+// accepts an object with student_id, enrollment_year, enrollment_quarter, graduation_year, graduation_quarter, and enrollments properties
+// enrollments is an array of objects with course_id, year, quarter, and grade properties
+// returns the student_id of the student whose enrollments were added
+export async function addEnrollments({student_id, enrollment_year, enrollment_quarter, graduation_year, graduation_quarter, enrollments}) {
+    const [result] = await pool.query(`
+        UPDATE student
+        SET enrollment_year = ?, enrollment_quarter = ?, graduation_year = ?, graduation_quarter = ?
+        WHERE student_id = ?
+        `, [enrollment_year, enrollment_quarter, graduation_year, graduation_quarter, student_id]);
+
+        for (let enrollment of enrollments) {
+            if (isNaN(enrollment.grade)) {
+                switch (enrollment.grade) {
+                    case 'A':
+                        enrollment.grade = 4.0;
+                        break;
+                    case 'A-':
+                        enrollment.grade = 3.7;
+                        break;
+                    case 'B+':
+                        enrollment.grade = 3.3;
+                        break;
+                    case 'B':
+                        enrollment.grade = 3.0;
+                        break;
+                    case 'B-':
+                        enrollment.grade = 2.7;
+                        break;
+                    case 'C+':
+                        enrollment.grade = 2.3;
+                        break;
+                    case 'C':
+                        enrollment.grade = 2.0;
+                        break;
+                    case 'C-':
+                        enrollment.grade = 1.7;
+                        break;
+                    case 'D+':
+                        enrollment.grade = 1.3;
+                        break;
+                    case 'D':
+                        enrollment.grade = 1.0;
+                        break;
+                    case 'E':
+                        enrollment.grade = 0.0;
+                        break;
+                    case 'P':
+                        enrollment.grade = 0.0;
+                        break;
+                    case 'NC':
+                        enrollment.grade = 0.0;
+                        break;
+                    case 'AU':
+                        enrollment.grade = 0.0;
+                        break;
+                    default:
+                        enrollment.grade = null;
+                        break;
+                }
+            }
+            
+            await pool.query(`
+                INSERT INTO enrollment (student_id, course_id, year, quarter, grade)
+                VALUES (?, ?, ?, ?, ?)
+                `, [student_id, enrollment.course_id, enrollment.year, enrollment.quarter, enrollment.grade]);
+        }
+
+        return result.insertId;
+}
+
+// get all enrollments for a user
+// accepts email as parameter
+// returns an object with three properties: current, past, and gpa
+// current is an array of courses the student is currently enrolled in
+// past is an array of courses the student has completed
+// gpa is the student's 4.0 grade point average
 export async function getEnrollments(email) {
-  const [rows] = await pool.query(
-    `
-        SELECT student.student_id AS student_id, enrollment.course_id AS course_id, name, description, credits, attributes, year, quarter, grade
+    const [current] = await pool.query(`
+        SELECT enrollment.course_id AS course_id, name, description, credits, attributes
         FROM student
         INNER JOIN enrollment ON student.student_id = enrollment.student_id
         INNER JOIN course ON enrollment.course_id = course.course_id
-        WHERE email = ?
-        `,
-    [email]
-  );
+        WHERE email = ? AND year = ${currentYear} AND quarter = "${currentQuarter()}"
+        `, [email]);
 
-  return rows;
-}
+    const [past]  = await pool.query(`
+        SELECT enrollment.course_id AS course_id, name, description, credits, attributes, year, quarter, grade
+        FROM student
+        INNER JOIN enrollment ON student.student_id = enrollment.student_id
+        INNER JOIN course ON enrollment.course_id = course.course_id
+        WHERE email = ? AND (year < ${currentYear} OR (year = ${currentYear} AND quarter < "${currentQuarter()}"))
+        `, [email]);
+    
+    let qualityPoints = 0;
+    let totalCredits = 0;
 
-export async function addStudent({ first_name, last_name, email, avatar }) {
-  const [result, error] = await pool.query(
-    `
-        INSERT INTO student (first_name, last_name, email, avatar)
-        VALUES (?, ?, ?, ?)
-        `,
-    [first_name, last_name, email, avatar]
-  );
+    let credits;
+    let grade;
+    for (let course of past) {
+        credits = parseInt(course.credits);
+        grade = parseFloat(course.grade);
+        if (!isNaN(grade) && !isNaN(credits)) {
+            totalCredits += credits;
+            qualityPoints += grade * credits;
+        }
+    }
+    const gpa = function() {
+        switch (totalCredits) {
+            case 0:
+                return null;
+            default:
+                return qualityPoints / totalCredits;
+        
+        }
+    }
 
-  if (error) {
-    console.error(error);
-    return -1;
-  }
-
-  return result.insertId;
+    return {'current': current, 'past': past, 'gpa': gpa()};
 }
