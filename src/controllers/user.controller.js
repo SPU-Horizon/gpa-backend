13fbb946fef5e26f3ae23961ffc20e97d0ea89c6
Matthew Negasi <negasim@spu.edu>
2024-04-09 20:16:09 -0700

    Initial Commit.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -12,23 +12,6 @@ export async function registerUserFunction(req, res) {
   }
 }
 
-export async function uploadProfilePhoto(req, res) {
-  const file = req.file;
-  console.log("Received file:", file);
-
-  // Handle the file as needed
-  console.log("Received file:", file);
-
-  try {
-    await fs.unlink(file.path);
-    console.log("File removed successfully.");
-  } catch (error) {
-    console.error("Error removing file:", error);
-  }
-
-  res.status(200).json({ message: "Profile photo uploaded successfully." });
-}
-
 export async function getInfo(req, res) {
   const studentInfo = await getUser(req.query.email);
   res.send(studentInfo);
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,13 +1,14 @@
 import express from "express";
 
-import { registerUserFunction, getInfo, uploadProfilePhoto } from "../controllers/user.controller.js";
+import {
+  registerUserFunction,
+  getInfo,
+} from "../controllers/user.controller.js";
 
 export const userRouter = express.Router();
 
 userRouter.post("/register", registerUserFunction);
 
 userRouter.get("/getUserInfo", getInfo);
-
-userRouter.post("/upload-profile-photo", uploadProfilePhoto);
 
 export default userRouter;
