ab74526af280e7b01436b9fe1d004064854b5989
Steven Aziz <90646854+azizsteven@users.noreply.github.com>
2024-04-23 22:12:34 -0700

    Update database schema, test user, and remove gpa_courses

--- gpa_test.sql
+++ gpa_test.sql
@@ -1,2 +1,2 @@
 INSERT INTO student (student_id, first_name, last_name, email, counselor_id, enrollment_year, enrollment_quarter, graduation_year, graduation_quarter)
-VALUE (1, 'John', 'Appleseed', 'john@spu.edu', 1, 2020, 'autumn', 2024, 'spring');
+VALUE (1, 'John', 'Appleseed', 'john@spu.edu', 4, 2020, 'autumn', 2024, 'spring');
