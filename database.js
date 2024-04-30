c8aa24de40d4ff91c0f90b2ba433a2d488d7fe2f
Steven Aziz <90646854+azizsteven@users.noreply.github.com>
2024-04-30 15:18:49 -0700

    Bugfix - mysql2 syntax

--- database.js
+++ database.js
@@ -332,7 +332,7 @@ export async function addStudentField(student_id, name, type, year, quarter, ud_
         FROM course
         WHERE course_id IN (?)
       `,
-      [courses_set.values()]);
+      [Array.from(courses_set)]);
   }
   catch (error) {
     console.log(error);
@@ -448,7 +448,7 @@ export async function createStudentPlan(max_credits_per_quarter, mandatory_cours
         FROM course
         WHERE course_id IN (?)
         `,
-        [curr_prerequisites.values()]
+        [Array.from(curr_prerequisites)]
       );
     }
     catch (error) {
